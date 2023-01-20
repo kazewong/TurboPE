29b793322de162d00eecd75f366fbb1f115b1752
Kaze Wong <kazewong.physics@gmail.com>
2023-01-20 11:37:27 -0500

    It should compiles now

--- src/scripts/plotEvents.py
+++ src/scripts/plotEvents.py
@@ -4,8 +4,8 @@ import corner
 import paths
 
 
-flowMC_data = np.load('../data/GW150914_flowMC.npz')
-bilby_data = np.genfromtxt('../data/GW150914_Bilby.dat')
+flowMC_data = np.load(paths.data / 'GW150914_flowMC.npz')
+bilby_data = np.genfromtxt(paths.data / 'GW150914_Bilby.dat')
 axis_labels=  [r'$M_c$', r'$q$', r'$\chi_1$', r'$\chi_2$', r'$d_{\rm{L}}$', r'$\phi_c$', r'$\iota$', r'$\psi$', 'RA', 'DEC']
 flowMC_chains = flowMC_data['chains'][:,:,[0,1,2,3,4,6,7,8,9,10]].reshape(-1,10)
 bilby_chains = bilby_data[1:,[1,0,2,3,6,11,9,10,8,7]]
@@ -17,8 +17,8 @@ fig = corner.corner(bilby_chains,fig=fig,color='C1',hist_kwargs={'density':True}
 
 fig.savefig(paths.figures / "GW150914.pdf", bbox_inches="tight")
 
-flowMC_data = np.load('../data/GW170817_flowMC.npz')
-bilby_data = np.genfromtxt('../data/GW170817_Bilby.dat')
+flowMC_data = np.load(paths.data / 'GW170817_flowMC.npz')
+bilby_data = np.genfromtxt(paths.data / 'GW170817_Bilby.dat')
 axis_labels=  [r'$M_c$', r'$q$', r'$\chi_1$', r'$\chi_2$', r'$d_{\rm{L}}$', r'$t_c$', r'$\phi_c$', r'$\iota$', r'$\psi$', 'RA', 'DEC']
 flowMC_chains = flowMC_data['chains'][:,:,[0,1,2,3,4,5,6,7,8,9,10]].reshape(-1,11)
 bilby_chains = bilby_data[1:,[0,1,2,3,4,10,9,11,8,6,5]]

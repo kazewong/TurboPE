21651707c274c52ae8e95ae8a39a4459b4a03926
Kaze Wong <kazewong.physics@gmail.com>
2023-01-21 16:37:26 -0500

    Fix plotting script

--- src/scripts/plotEvents.py
+++ src/scripts/plotEvents.py
@@ -19,12 +19,11 @@ fig.savefig(paths.figures / "GW150914.pdf", bbox_inches="tight")
 
 flowMC_data = np.load(paths.data / 'GW170817_flowMC_1800.npz')
 bilby_data = np.genfromtxt(paths.data / 'GW170817_Bilby_flat.dat')
-axis_labels=  [r'$M_c$', r'$q$', r'$\chi_1$', r'$\chi_2$', r'$d_{\rm{L}}$', r'$t_c$', r'$\phi_c$', r'$\iota$', r'$\psi$', 'RA', 'DEC']
-flowMC_chains = flowMC_data['chains'][:,:,[0,1,2,3,4,5,6,7,8,9,10]].reshape(-1,11)
-bilby_chains = bilby_data[1:,[0,1,2,3,4,10,9,11,8,6,5]]
-flowMC_chains[:,7] = np.arccos(flowMC_chains[:,7])
-flowMC_chains[:,10] = np.arcsin(flowMC_chains[:,10])
-bilby_chains[:,5] = bilby_chains[:,5] - 1187008882.4
+axis_labels=  [r'$M_c$', r'$q$', r'$\chi_1$', r'$\chi_2$', r'$d_{\rm{L}}$', r'$\phi_c$', r'$\iota$', r'$\psi$', 'RA', 'DEC']
+flowMC_chains = flowMC_data['chains'][:,:,[0,1,2,3,4,6,7,8,9,10]].reshape(-1,10)
+bilby_chains = bilby_data[1:,[0,1,2,3,4,8,9,7,6,5]]
+flowMC_chains[:,6] = np.arccos(flowMC_chains[:,6])
+flowMC_chains[:,9] = np.arcsin(flowMC_chains[:,9])
 
 fig = corner.corner(flowMC_chains,color='C0',labels=axis_labels,show_titles=True,hist_kwargs={'density':True})
 fig = corner.corner(bilby_chains,fig=fig,color='C1',hist_kwargs={'density':True})

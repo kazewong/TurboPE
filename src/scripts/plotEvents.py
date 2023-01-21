6853615f7cd995f7ede25938e54388be1a61c22d
Kaze Wong <kazewong.physics@gmail.com>
2023-01-21 16:24:03 -0500

    Update BNS result

--- src/scripts/plotEvents.py
+++ src/scripts/plotEvents.py
@@ -17,8 +17,8 @@ fig = corner.corner(bilby_chains,fig=fig,color='C1',hist_kwargs={'density':True}
 
 fig.savefig(paths.figures / "GW150914.pdf", bbox_inches="tight")
 
-flowMC_data = np.load(paths.data / 'GW170817_flowMC.npz')
-bilby_data = np.genfromtxt(paths.data / 'GW170817_Bilby.dat')
+flowMC_data = np.load(paths.data / 'GW170817_flowMC_1800.npz')
+bilby_data = np.genfromtxt(paths.data / 'GW170817_Bilby_flat.dat')
 axis_labels=  [r'$M_c$', r'$q$', r'$\chi_1$', r'$\chi_2$', r'$d_{\rm{L}}$', r'$t_c$', r'$\phi_c$', r'$\iota$', r'$\psi$', 'RA', 'DEC']
 flowMC_chains = flowMC_data['chains'][:,:,[0,1,2,3,4,5,6,7,8,9,10]].reshape(-1,11)
 bilby_chains = bilby_data[1:,[0,1,2,3,4,10,9,11,8,6,5]]
